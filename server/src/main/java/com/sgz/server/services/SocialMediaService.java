47bcb82428b3fa9a02e9e723b80f971896e5c632
Zun <zunsamuel97@gmail.com>
2020-08-17 22:12:43 -0500

    working on social media service impl

--- server/src/main/java/com/sgz/server/services/SocialMediaService.java
+++ server/src/main/java/com/sgz/server/services/SocialMediaService.java
@@ -1,4 +1,31 @@
 package com.sgz.server.services;
 
+import com.sgz.server.entities.SocialMedia;
+import com.sgz.server.exceptions.InvalidIdException;
+import com.sgz.server.repos.SocialMediaRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
 public class SocialMediaService {
+
+    private final SocialMediaRepo socialMediaRepo;
+
+    @Autowired
+    public SocialMediaService(SocialMediaRepo socialMediaRepo) {
+        this.socialMediaRepo = socialMediaRepo;
+    }
+
+    public SocialMedia getSocialMediaById(UUID id) throws InvalidIdException {
+        Optional<SocialMedia> toGet = socialMediaRepo.findById(id);
+
+        if(toGet.isEmpty()) {
+            throw new InvalidIdException("Invalid Id");
+        }
+
+        return toGet.get();
+    }
 }
