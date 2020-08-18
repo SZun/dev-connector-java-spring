f4b68613f87336416c2cae87c5503b63538c4fb2
Zun <zunsamuel97@gmail.com>
2020-08-17 20:44:05 -0500

    updated skills and social medias repos and tested all implementation of both

--- server/src/main/java/com/sgz/server/repos/SkillRepo.java
+++ server/src/main/java/com/sgz/server/repos/SkillRepo.java
@@ -4,9 +4,14 @@ import com.sgz.server.entities.Skill;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface SkillRepo extends JpaRepository<Skill, UUID> {
+
+    boolean existsByName(String name);
+
+    Optional<Skill> findByName(String name);
 
 }
--- server/src/main/java/com/sgz/server/repos/SocialMediaRepo.java
+++ server/src/main/java/com/sgz/server/repos/SocialMediaRepo.java
@@ -4,9 +4,14 @@ import com.sgz.server.entities.SocialMedia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface SocialMediaRepo extends JpaRepository<SocialMedia, UUID> {
+
+    boolean existsByLink(String link);
+
+    Optional<SocialMedia> findByLink(String link);
 
 }
